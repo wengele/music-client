16b3e0bcfd52bf170a59b4b02373a348f8d6de9c
Wengelh <106118460+Wengelh@users.noreply.github.com>
2023-05-20 13:15:13 -0500

    INISTAL COMMIT

--- music.js
+++ music.js
@@ -1,32 +1,31 @@
 window.onload = function () {
     document.getElementById("loginBtn").onclick = login;
-}
+};
 
 async function login() {
-    const response = await fetch('http://localhost:3000/api/auth/login', {
-        method: 'POST',
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    const response = await fetch("http://localhost:3000/api/auth/login", {
+        method: "POST",
         body: JSON.stringify({
-            "username": document.getElementById('username').value,
-            "password": document.getElementById('Password').value
+            username,
+            password,
         }),
         headers: {
-            'Content-Type': 'application/json'
-        }
+            "Content-Type": "application/json",
+        },
     });
 
-
-    const result = await response.json();
-
-    if (result.ok) {
-        document.getElementById("errors").innerText = result.message;
-        //"Incorrect username and password";
-        //const result = await response.json();
-
-
-    } else {
+    console.log(response);
+    if (response.ok) {
+        const result = await response.json();
+        console.log(result);
         sessionStorage.setItem("token", result.accessToken);
         sessionStorage.setItem("username", result.username);
+
+    } else {
+        document.getElementById("errorMsg").innerText =
+            "Incorrect username and password";
     }
-
-
 }
